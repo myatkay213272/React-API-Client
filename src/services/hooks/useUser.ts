9d5bb27fd68df8fcb55df8d42ae3213d406b0829
myatkayzt <myatthant68@gmail.com>
2025-04-23 21:30:31 +0630

    Update useUser.ts
    add proper error state management in useUsers

--- src/services/hooks/useUser.ts
+++ src/services/hooks/useUser.ts
@@ -1,41 +1,34 @@
-import { useState,useEffect } from "react"
-import userService,{User} from "../user-service"
-import { CanceledError } from "../api-client";
-import { AxiosError } from "axios";
+import { useState ,useEffect} from "react"
+import { User }from "../services/user-service"
+import {CanceledError} from '../services/api-client'
+import userService from "../services/user-service"
 
 
-const useUsers = ()=>{
+const useUsers = () => {
+ 
+const [users,setUsers] = useState<User[]>([])
+  const [error,setError] = useState('')
+  const [isLoading,setLoading] = useState(true)
 
-      const [users,setUsers] = useState<User[]>([])
-      const [error,setError] = useState('')
-      const [isLoading,setIsLoading] = useState(true)
-    
-      useEffect(()=>{
-    
-            setIsLoading(true)
-    
-            const {request,cancel} =  userService.getAll<User>()
-            request
-            .then((res)=>{
-              setUsers(res.data)
-              setIsLoading(false)
-            })
-          
-          .catch((err)=>{
-    
-            if(err instanceof CanceledError) return
-    
-            setError((err as AxiosError).message)
-            setIsLoading(false)
-          })
-        
-        return ()=> cancel()
-    
-        //get-> await promis -> res/err
-    
-      },[])
 
-      return {users,error,isLoading,setUsers,setError}
+  useEffect(()=>{
+
+    const {request,cancel} =userService.getAll<User>()
+    request.then(res=>{
+      setUsers(res.data)
+      setLoading(false)
+    })
+    .catch(err=>{
+      if(err instanceof CanceledError) return
+      setError(err.message)
+      setLoading(false)
+    })
+
+    return ()=>cancel()
+
+  },[])
+
+  return {users,error,isLoading,setError,setUsers}
 
 }
 
