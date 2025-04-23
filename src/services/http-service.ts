1f39fc426a58c2ec024b6e4b2440e96270b383bc
myatkayzt <myatthant68@gmail.com>
2025-04-23 21:28:26 +0630

    Update http-service.ts
    fix: handle API error correctly in http-service

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -1,43 +1,48 @@
 import apiClient from "./api-client";
-// import { AxiosRequestConfig, CanceledError } from "axios";//extra
 
 interface Entity {
-    id:number
+    id : number
 }
+
 
 class HttpService{
 
-    constructor(endpoint:string){
+    endpoint : string
+
+    constructor(endpoint : string){
         this.endpoint = endpoint
     }
 
     getAll<T>(){
-       
+
         const controller = new AbortController()
 
-        const request= apiClient.get<T[]>(this.endpoint, {
-            signal : controller.signal,
-            // ...config
-        })  
-        
-        return { request,cancel:()=>controller.abort()} }
+        const request = apiClient.get<T[]>(this.endpoint,{
+            signal : controller.signal
+        })
 
-
-     delete(id){
-        return  apiClient.delete(this.endpoint + "/" + id)
-     }
-
-     create<T>(entity:T){
-        return apiClient.post(this.endpoint,entity)
-     }
-
-     update<T extends Entity>(entity:T){
-       return apiClient.patch(this.endpoint + "/" + entity.id,entity)
-     }
-
+        return {request,cancel: ()=>controller.abort()}
 
     }
 
-const Create = (endpoint : string)=> new HttpService(endpoint)
 
-export default Create
+
+    delete(id : number){
+       return apiClient.delete(this.endpoint +'/' + id)
+    }
+
+
+    create<T>(entity : T){
+       return apiClient.post(this.endpoint ,entity)
+    }
+
+
+
+    update<T extends Entity>(entity : T){
+       return apiClient.patch(this.endpoint +'/'+ entity.id,entity)
+    }
+}
+
+const create = (endpoint : string) => new HttpService(endpoint)
+
+export default create
